d854577052053da5d610101c58fb2b0e3c1e26c2
zinbrox <53508807+zinbrox@users.noreply.github.com>
2021-02-15 19:51:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:books_app/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => Home(),
+      },
+    );
+  }
+}
